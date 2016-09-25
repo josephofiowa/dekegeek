5865ffbb61087ad04d68c1a2bab31965a510a3e0
Joseph Nelson <joseph.nelson2012@gmail.com>
2016-09-24 20:56:23 -0400

    added logo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# dekegeek
+# DekeGeek
+
+
--- boshac16/code/boshac.py
+++ boshac16/code/boshac.py
@@ -5,12 +5,26 @@ Created on Sat Sep 24 18:25:22 2016
 @author: JosephNelson
 """
 
-blocks = pd.read_csv('./20152016_02_total_blocks.csv')
+# loading data
+blocks = pd.read_csv('./assets/data/20152016_02_total_blocks.csv')
 blocks.head()
 blocks.describe()
 
-cf = pd.read_csv('./20152016_02_total_cf.csv')
+cf = pd.read_csv('./assets/data/20152016_02_total_cf.csv')
 
-faceoffs = pd.read_csv('./20152016_02_total_faceoff.csv')
+faceoffs = pd.read_csv('./assets/data/20152016_02_total_faceoff.csv')
 
-giveaways = pr.read_csv('./20152016_02_total_giveaways.csv')
+giveaways = pd.read_csv('./assets/data/20152016_02_total_giveaways.csv')
+
+goals = pd.read_csv('./assets/data/20152016_02_total_goals.csv')
+
+hits = pd.read_csv('./assets/data/20152016_02_total_hits.csv')
+
+miss = pd.read_csv('./assets/data/20152016_02_total_miss.csv')
+
+pen_type = pd.read_csv('./assets/data/20152016_02_total_pen_type.csv')
+
+'./assets/data/20152016_02_total_pen.csv'
+'./assets/data/20152016_02_total_shot.csv'
+'./assets/data/20152016_02_total_take.csv'
+
